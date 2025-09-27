f27e61297b68e166f95bc0c3476a804e3399208e
k0xdama <prioux.mateo@outlook.com>
2025-09-27 05:08:16 +0200

    init.sh : can't create a distant root for host

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -6,5 +6,37 @@ service mariadb start || {
 	exit 1
 }
 
+sleep 2
 
-exec /bin/bash
+mysql -e "CREATE DATABASE IF NOT EXISTS mariadb;" || {
+	echo "Impossible de créer une base de données !"
+       	exit 1 
+}
+
+mysql -e "CREATE USER 'root'@'172.17.0.1' IDENTIFIED by 'hoothoot';" || {
+	echo "Impossible de créer un utilisateur root pour l'hôte !"
+	exit 1
+}
+
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.17.0.1' with GRANT OPTIONS;" || {
+	echo "Impossible d'attribuer tout les droits à l'utilisateur root de l'hôte !"
+	exit 1
+}
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'hoothoot';" || {
+	echo "Impossible de définir un mot de passe pour ROOT !"
+       	exit 1 
+}
+
+mysql -u root -p"hoothoot" -e "CREATE USER 'bird'@'localhost' IDENTIFIED BY 'cuicui';" || {
+	echo "Impossible de créer l'utilisateur BIRD !"
+       	exit 1 
+}
+
+mysql -u root -p"hoothoot" -e "GRANT ALL PRIVILEGES ON mariadb.* TO 'bird'@'localhost' IDENTIFIED BY 'cuicui';" || {
+	echo "Impossible de donner les privilèges sur la base de données MARIADB à BIRD !"
+	exit 1 
+}
+
+mysqladmin -u root -p"hoothoot" shutdown
+exec mysqld_safe
