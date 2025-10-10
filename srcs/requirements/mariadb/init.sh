54401b5cd7fb8171df5c0458550d61f89b9e316e
k0xdama <prioux.mateo@outlook.com>
2025-10-10 23:25:23 +0200

    syntax error

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+MARIADB_DATABASE=wordpress
+MARIADB_ROOT_PASSWORD=hoothoot
+MARIADB_USER=canary
+MARIADB_PASSWORD=cuicui
+
 echo -e "init.sh has been runned :)"
 service mariadb start || {
 	echo "Impossible de démarrer MARIA-DB !"
@@ -8,12 +13,12 @@ service mariadb start || {
 
 sleep 2
 
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};" || {
+mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;" || {
 	echo "Impossible de créer une base de données !"
        	exit 1 
 }
 
-mysql -e "CREATE USER IF NOT EXISTS 'root'@'172.17.0.1' IDENTIFIED by ${MARIADB_ROOTPASSWORD};" || {
+mysql -e "CREATE USER IF NOT EXISTS 'root'@'172.17.0.1' IDENTIFIED by '${MARIADB_ROOT_PASSWORD}';" || {
 	echo "Impossible de créer un utilisateur root pour l'hôte !"
 	exit 1
 }
@@ -23,20 +28,20 @@ mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.17.0.1' with GRANT OPTION;"
 	exit 1
 }
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY ${MARIADB_ROOTPASSWORD};" || {
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';" || {
 	echo "Impossible de définir un mot de passe pour ROOT !"
        	exit 1 
 }
 
-mysql -u root -p"hoothoot" -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}@'172.17.0.1' IDENTIFIED BY ${MARIADB_PASSWORD};" || {
+mysql -u root -p"hoothoot" -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}@'172.17.0.1' IDENTIFIED BY '${MARIADB_PASSWORD}';" || {
 	echo "Impossible de créer l'utilisateur BIRD !"
        	exit 1 
 }
 
-mysql -u root -p"hoothoot" -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO 'bird'@'172.17.0.1' IDENTIFIED BY ${MARIADB_PASSWORD};" || {
+mysql -u root -p"hoothoot" -e "GRANT ALL PRIVILEGES ON '${MARIADB_DATABASE}'.* TO ${MARIADB_USER}@'172.17.0.1' IDENTIFIED BY '${MARIADB_PASSWORD}';" || {
 	echo "Impossible de donner les privilèges sur la base de données MARIADB à BIRD !"
 	exit 1 
 }
 
-mysqladmin -u root -p"${MARIADB_ROOTPASSWORD}" shutdown
+mysqladmin -u root -p"${MARIADB_ROOT_PASSWORD}" shutdown
 exec mysqld_safe
