df2163f905c4bbbbff94fb4fa6a740606b1e4789
k0xdama <prioux.mateo@outlook.com>
2025-10-10 22:40:07 +0200

    need to push

--- srcs/requirements/mariadb/init.sh
+++ srcs/requirements/mariadb/init.sh
@@ -8,12 +8,12 @@ service mariadb start || {
 
 sleep 2
 
-mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;" || {
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};" || {
 	echo "Impossible de créer une base de données !"
        	exit 1 
 }
 
-mysql -e "CREATE USER IF NOT EXISTS 'root'@'172.17.0.1' IDENTIFIED by 'hoothoot';" || {
+mysql -e "CREATE USER IF NOT EXISTS 'root'@'172.17.0.1' IDENTIFIED by ${MARIADB_ROOTPASSWORD};" || {
 	echo "Impossible de créer un utilisateur root pour l'hôte !"
 	exit 1
 }
@@ -23,20 +23,20 @@ mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.17.0.1' with GRANT OPTION;"
 	exit 1
 }
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'hoothoot';" || {
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY ${MARIADB_ROOTPASSWORD};" || {
 	echo "Impossible de définir un mot de passe pour ROOT !"
        	exit 1 
 }
 
-mysql -u root -p"hoothoot" -e "CREATE USER IF NOT EXISTS 'bird'@'localhost' IDENTIFIED BY 'cuicui';" || {
+mysql -u root -p"hoothoot" -e "CREATE USER IF NOT EXISTS ${MARIADB_USER}@'172.17.0.1' IDENTIFIED BY ${MARIADB_PASSWORD};" || {
 	echo "Impossible de créer l'utilisateur BIRD !"
        	exit 1 
 }
 
-mysql -u root -p"hoothoot" -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'bird'@'localhost' IDENTIFIED BY 'cuicui';" || {
+mysql -u root -p"hoothoot" -e "GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO 'bird'@'172.17.0.1' IDENTIFIED BY ${MARIADB_PASSWORD};" || {
 	echo "Impossible de donner les privilèges sur la base de données MARIADB à BIRD !"
 	exit 1 
 }
 
-mysqladmin -u root -p"hoothoot" shutdown
+mysqladmin -u root -p"${MARIADB_ROOTPASSWORD}" shutdown
 exec mysqld_safe
--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -2,20 +2,18 @@
 
 echo  "init.sh has been runned :)"
 
-wp config create --allow-root --path=/var/www/html/wordpress --dbname=wordpress --dbuser=canary --dbpass=cuicui \
-	--dbhost=mariadb:3306 --dbprefix=wp_ || {
+wp config create --allow-root --path=/var/www/html/wordpress --dbname=${MARIADB_DATABASE} --dbuser=${MARIADB_USER} \ 
+	--dbpass=${MARIADB_PASSWORD} --dbhost=mariadb:3306 --dbprefix=wp_ || {
 
 	echo "Impossible de configurer wordpress !"
 	exit 1
 }
 
-wp core install --allow-root --url=pmateo.42.fr --title=Pmateo_Website --admin_user=pmateo \
-	--admin_password =imyummyotp --admin_email=pmateo@student.42paris.fr || {
+wp core install --allow-root --url=pmateo.42.fr --title=Pmateo_Website --admin_user=${WP_ADM} \
+	--admin_password =${WP_ADM_PASSWORD} --admin_email=${WP_ADM_EMAIL} || {
 	
 	echo "Impossible de paramétrer le site web !"
 	exit 1
 }
 
 echo "Wordpress has been succesfully installed !"
-
-exec /bin/bash
