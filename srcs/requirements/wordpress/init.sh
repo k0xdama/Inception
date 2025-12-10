c6669f052a9e9f7e95b1f9da7f5d9083ba096f95
k0xdama <prioux.mateo@outlook.com>
2025-12-10 16:05:53 +0100

    fix wp init.sh, added env var

--- srcs/requirements/wordpress/init.sh
+++ srcs/requirements/wordpress/init.sh
@@ -10,12 +10,12 @@ fi
 
 if [ ! -f "/var/www/html/wordpress/wp-config.php" ]; then
 	wp config create --path=/var/www/html/wordpress --dbname=${MARIADB_DATABASE} --dbuser=${MARIADB_USER} \
-		--dbpass=${MARIADB_USER_PASSWORD} --dbhost=mariadb:3306 --dbprefix=wp_ --dbcharset=utf8
+		--dbpass=${MARIADB_USER_PASSWORD} --dbhost=${WP_DB_HOST} --dbprefix=wp_ --dbcharset=utf8
 	echo "Wordpress database has been configured !"
 fi
 
 if ! wp core is-installed --path=/var/www/html/wordpress 2>/dev/null; then
-	wp core install --path=/var/www/html/wordpress --url=pmateo.42.fr --title=Pmateo_Website --admin_user=${WP_ADM} \
+	wp core install --path=/var/www/html/wordpress --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADM} \
 		--admin_password=${WP_ADM_PASSWORD} --admin_email=${WP_ADM_EMAIL}
 	wp user create ${WP_OTH_USER} ${WP_OTH_EMAIL} --path=/var/www/html/wordpress --user_pass=${WP_OTH_PASSWORD}
 	wp theme install twentytwentyfour --path=/var/www/html/wordpress --activate --allow-root
